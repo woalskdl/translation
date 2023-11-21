6894b1fc2cd2e19a1f2251967f502e23624f661f
jay nam <woalskdl@gmail.com>
2023-11-21 23:09:53 +0900

    expo-localization, i18n 설치 및 테스트 (i18n 은 버전문제로 4.2.0으로 다운그레이드)

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getLocales } from "expo-localization";
+import { I18n } from "i18n-js";
+
+const i18n = new I18n({
+  en : { welcome: 'Hello' },
+  ko : { welcome: '안녕하세요' }
+})
+
+const deviceLanguage = getLocales()[1].languageCode;
+i18n.locale = deviceLanguage;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{deviceLanguage}</Text>
+      <Text>{i18n.t('welcome')}</Text>
       <StatusBar style="auto" />
     </View>
   );
